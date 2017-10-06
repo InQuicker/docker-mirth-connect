b868402febb2dd09182c77e14331449d4404fb22
Brian Shirai <brixen@gmail.com>
2017-10-06 16:41:03 -0700

    Fix docker-entrypoint.sh.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,12 +2,12 @@
 
 set -e
 
-traefik &
-
 if [ "$1" = 'java' ]; then
   [ -x /docker-mirth-preexec.sh ] && /docker-preexec.sh
-  [ -x /docker-mirth-exec.sh ] && /docker-mirth-exec.sh
+  [ -x /docker-mirth-exec.sh ] && /docker-mirth-exec.sh "$@"
   [ -x /docker-mirth-postexec.sh ] && ./docker-mirth-postexec.sh
-fi
 
-exec "$@"
+  exec traefik
+else
+  exec "$@"
+fi
