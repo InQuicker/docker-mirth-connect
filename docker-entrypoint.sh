d819325438e19cee21e4465934c860d4d3f4f732
Brian Shirai <brixen@gmail.com>
2017-10-09 18:13:26 -0700

    Add synchronization.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,11 +3,27 @@
 set -e
 
 if [ "$1" = 'java' ]; then
+  traefik &
+
+  # Wait for Traefik to start before starting backend
+  while ! traefik healthcheck; do
+    sleep 0.5
+  done
+
   [ -x /docker-mirth-preexec.sh ] && /docker-mirth-preexec.sh
   [ -x /docker-mirth-exec.sh ] && /docker-mirth-exec.sh "$@"
+
+  # Wait for Mirth to start before running postexec
+  while ! curl -s http://localhost:3000/api/server/version; do
+    sleep 0.5
+  done
+
   [ -x /docker-mirth-postexec.sh ] && /docker-mirth-postexec.sh
 
-  exec traefik
+  # Exit if Traefik becomes unresponsive
+  while traefik healthcheck; do
+    sleep 60
+  done
 else
   exec "$@"
 fi
