893bf4c7d0a80755238b593efbc3052d4cda0699
Brian Shirai <brixen@gmail.com>
2017-10-09 18:32:45 -0700

    Better synchronization for Mirth starting.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,8 +14,8 @@ if [ "$1" = 'java' ]; then
   [ -x /docker-mirth-exec.sh ] && /docker-mirth-exec.sh "$@"
 
   # Wait for Mirth to start before running postexec
-  while ! curl -s http://localhost:3000/api/server/version; do
-    sleep 0.5
+  while ! curl -s http://localhost:8080/api/server/version > /dev/null 2>&1; do
+    sleep 1
   done
 
   [ -x /docker-mirth-postexec.sh ] && /docker-mirth-postexec.sh
--- docker-mirth-exec.sh
+++ docker-mirth-exec.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo "Mirth starting..."
+
 chown -R mirth /opt/mirth-connect/appdata
 
 gosu mirth "$@" &
